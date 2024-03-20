9801462df915f968d079ac689a4081f196154422
Øystein Håberg <islandstone89@gmail.com>
2024-03-21 00:31:24 +0100

    * Added form validation * Added error icon

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
 /* Select the form */
-let form = document-querySelector(".form");
+let form = document.querySelector(".form");
+
+/* Select the email input */
+let input = document.querySelector(".js-email-input");
+
+/* Seelct the error icon */
+let errorIcon = document.querySelector(".icon-error");
 
 /* Select the error message */
 let errorMessage = document.querySelector(".js-error");
@@ -9,5 +15,29 @@ let errorMessage = document.querySelector(".js-error");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    errorMessage.computedStyleMap.display = "block";
+
+    if (input.value === "") {
+        errorIcon.style.display = "block";
+        errorMessage.style.display = "block";
+        errorMessage.textContent = "You need to enter an email address";
+        input.style.borderColor = "var(--clr-red)"
+        input.style.borderWidth = "2px";
+    }
+    
+    else if (input.value.indexOf("@") === -1) {
+        errorIcon.style.display = "block";
+        errorMessage.style.display = "block";
+        errorMessage.textContent = "Please provide a valid email address";
+        input.style.borderColor = "var(--clr-red)";
+        input.style.borderWidth = "2px";
+    }
+
+    else {
+        errorIcon.style.display = "none";
+        errorMessage.style.display = "block";
+        errorMessage.textContent = "Success!";
+        errorMessage.style.color = "var(--clr-green";
+        input.style.borderColor = "var(--clr-green)";
+        input.style.borderWidth = "2px";
+    }
 })
